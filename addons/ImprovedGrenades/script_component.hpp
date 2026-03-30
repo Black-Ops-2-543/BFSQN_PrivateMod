40877a090a640a9bb7397ce55f1aa12136fc57e3
sgtobliterator <sgtobliterator@gmail.com>
2026-03-30 16:41:51 -0400

    Improved Grenades

---
+++ addons/ImprovedGrenades/script_component.hpp
@@ -0,0 +1,9 @@
+#define SPOKE "Blackforce Private Mod - Improved Grenades"
+#define COMPONENT ImprovedGrenades
+
+#define PREFIX BFSQN
+#define REQUIRED_VERSION 2.12
+#define AUTHOR "Blackforce Squadron"
+
+#define DOUBLES(a,b) a##_##b
+#define ADDON DOUBLES(PREFIX,COMPONENT)
